42652ba94661b79e5e2834f3663fffe45b12f074
niuxuerui <niuxuerui@aliyun.com>
2018-11-07 16:26:04 +0800

    66

---
+++ build/precache-manifest.e1c5f94c4ce3d19cff4aff52345a5efd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e1600915df1f9bd8c67c",
+    "url": "/static/js/main.e1600915.chunk.js"
+  },
+  {
+    "revision": "c73b33ebde5fa6503a73",
+    "url": "/static/js/1.c73b33eb.chunk.js"
+  },
+  {
+    "revision": "e1600915df1f9bd8c67c",
+    "url": "/static/css/main.77965ad1.chunk.css"
+  },
+  {
+    "revision": "710ac801ea51ca86f02a71e2cfca6ea6",
+    "url": "/index.html"
+  }
+];
